880d89840c32a025be08b4180567ce60cd4290ca
HenryCLH <henrychenglh@gmail.com>
2019-11-26 17:10:11 -0500

    bugfix

--- gamestates/mainmenu.lua
+++ gamestates/mainmenu.lua
@@ -1,6 +1,45 @@
 mainMenu = Gamestate.new()
 
 function mainMenu:enter()
+	local fullscreen = love.window.getFullscreen( )
+	mainMenuGrp = Menu.new()
+
+	mainMenuGrp:addItem{
+		name = 'Start Game',
+		action = function()
+			Gamestate.switch(gameLevel1)
+		end
+	}
+	mainMenuGrp:addItem{
+		name = 'Disable Fullscreen',
+		action = function(self)
+			if fullscreen then
+				fullscreen = false
+				self.name = 'Enable Fullscreen'
+				love.window.setFullscreen(fullscreen)
+			else
+				fullscreen = true
+				self.name = 'Disable Fullscreen'
+				love.window.setFullscreen( fullscreen)
+			end
+			settings[1] = fullscreen
+			success, message = love.filesystem.write('settings.txt', serialize(settings))
+			print('Save Settings. Saved? '..tostring(success))
+		end
+	}
+	mainMenuGrp:addItem{
+		name = 'Settings',
+		action = function()
+			Gamestate.push(settingsMenu)
+		end
+	}
+	mainMenuGrp:addItem{
+		name = 'Quit',
+		action = function()
+			love.event.push('quit')
+		end
+	}
+	
 	-- write default settings
 	settings = {false, 1, 1, "left", "right", "up"}
 	-- get custom settings from file
@@ -22,42 +61,6 @@ function mainMenu:enter()
 	else
 		music:play(music)
 	end
-	
-	local fullscreen = love.window.getFullscreen( )
-	mainMenuGrp = Menu.new()
-	mainMenuGrp:addItem{
-		name = 'Start Game',
-		action = function()
-			Gamestate.switch(gameLevel1)
-		end
-	}
-	mainMenuGrp:addItem{
-		name = 'Disable Fullscreen',
-		action = function(self)
-			if fullscreen then
-				fullscreen = false
-				self.name = 'Enable Fullscreen'
-				love.window.setFullscreen(fullscreen)
-			else
-				fullscreen = true
-				self.name = 'Disable Fullscreen'
-				love.window.setFullscreen( fullscreen)
-			end
-			settings[1] = fullscreen
-		end
-	}
-	mainMenuGrp:addItem{
-		name = 'Settings',
-		action = function()
-			Gamestate.push(settingsMenu)
-		end
-	}
-	mainMenuGrp:addItem{
-		name = 'Quit',
-		action = function()
-			love.event.push('quit')
-		end
-	}
 end
 
 function mainMenu:update(dt)
